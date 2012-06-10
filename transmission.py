35edb1f62917a4029e86b285ee4e048f01523613
Eric Davis <ed@npri.org>
2012-06-10 15:25:59 -0700

    Beginnings of a Django-style ORM

--- transmission.py
+++ transmission.py
@@ -1,8 +1,8 @@
 import base64
 import anyjson
+import requests
 import datetime
 import operator
-from constants import TORRENT_GET_FIELDS
 
 CSRF_ERROR_CODE = 409
 CSRF_HEADER = 'X-Transmission-Session-Id'
@@ -10,6 +10,7 @@ CSRF_HEADER = 'X-Transmission-Session-Id'
 RPC_HOST, RPC_PORT, RPC_PATH = ('localhost', 9091, '/transmission/rpc')
 
 class BadRequest(Exception): pass
+class BadOperator(Exception): pass
 
 class Client(object):
     def __init__(self, host=RPC_HOST, port=RPC_PORT, path=RPC_PATH):
@@ -22,57 +23,118 @@ class Client(object):
         if response.status_code == CSRF_ERROR_CODE:
             self.headers[CSRF_HEADER] = response.headers[CSRF_HEADER]
             return self.request(method, **kwargs)
-        return self._deserialize_response(response, method)
+        return self._deserialize_response(response)
 
     def _format_request_body(self, method, **kwargs):
         """
         Create a request object to be serialized and sent to Transmission.
         """
-        ids = kwargs.get('ids')
-        if ids is not None:
-            kwargs['ids'] = ids if isinstance(ids, (list, tuple)) else [ids]
-        return {"method": method, "arguments": kwargs}
+        body = {"method": method}
+        if 'tag' in kwargs:
+            body['tag'] = kwargs.pop('tag')
+        body.update({"arguments": kwargs})
+        return body
 
-    def _deserialize_response(self, response, method):
+    def _deserialize_response(self, response):
         doc = anyjson.deserialize(response.content)
-
         if doc['result'] != 'success':
             raise BadRequest("Request failed: '%s'" % doc['result'])
-
-        convert_map = {
-            "activityDate": datetime.datetime.fromtimestamp,
-            "addedDate": datetime.datetime.fromtimestamp,
-            "dateCreated": datetime.datetime.fromtimestamp,
-            "doneDate": datetime.datetime.fromtimestamp,
-            "startDate": datetime.datetime.fromtimestamp,
-            "pieces": lambda p: bytearray(base64.b64decode(p)),
-        }
-
-        if method == "torrent-get":
-            for info in doc["arguments"]["torrents"]:
-                for (key, value) in info.items():
-                    convert_func = convert_map.get(key)
-                    if convert_func is not None:
-                        info[key] = convert_func(value)
         return doc
 
-class Torrent(object):
-    def __init__(self, info):
-        self.info = info
+class TorrentIterator(object):
+    def __init__(self, client):
+        self.client = client
+        self.torrents = []
 
-    def __getattr__(self, key):
-        return self.info[key]
+    def filter(self, ids, predicates):
+        # Always include the 'name' and 'id' fields
+        fields = [p[0] for p in predicates]
 
-    def __repr__(self):
-        return r'<Torrent "%s">' % self.name.encode('utf-8')
+        request_params = {'fields': fields}
+        if ids:
+            request_params['ids'] = ids
+
+        response = self.client.request('torrent-get', **request_params)
+
+        for torrent in response['arguments']['torrents']:
+            predicate_results = []
+            for (key, op, value) in predicates:
+
+                # Do case-insensitive filtering on string values
+                try:
+                    torrent_value = torrent[key].lower()
+                    test_value = value.lower()
+                except AttributeError:
+                    torrent_value = torrent[key]
+                    test_value = value
+
+                ret = op(torrent_value, test_value)
+
+                predicate_results.append(ret)
+                if not ret: break
+
+            if all(predicate_results):
+                self.torrents.append(torrent)
+
+        return self
+
+    def all(self):
+        fields = ['id', 'name']
+        response = self.client.request('torrent-get', fields=fields)
+        self.torrents = response['arguments']['torrents']
+        return self
+
+    def __len__(self):
+        return len(self.torrents)
+
+    def __iter__(self):
+        for torrent in self.torrents:
+            yield torrent
 
 class Transmission(object):
     def __init__(self, host=RPC_HOST, port=RPC_PORT, path=RPC_PATH):
         self.client = Client(host, port, path)
 
-    def get(self, *ids):
-        response = self.client.request("torrent-get", ids=ids, fields=TORRENT_GET_FIELDS)
-        torrents = []
-        for info in response["arguments"]["torrents"]:
-            torrents.append(Torrent(info))
-        return torrents
+    def all(self):
+        return TorrentIterator(self.client).all()
+
+    def __iter__(self):
+        return iter(self.all())
+
+    def __len__(self):
+        return len(self.all())
+
+    def active(self):
+        return self.filter(ids='recently-active')
+
+    def get(self, **kwargs):
+        torrents = iter(self.filter(**kwargs))
+        try:
+            item = next(torrents)
+        except StopIteration:
+            item = None
+        return item
+
+    def filter(self, **kwargs):
+        predicates = []
+        ids = kwargs.pop('ids', None)
+
+        for key, value in kwargs.iteritems():
+            if '__' in key:
+                (field, key_op) = key.split('__')
+            else:
+                (field, key_op) = (key, 'eq')
+
+            try:
+                op = getattr(operator, key_op)
+            except AttributeError:
+                raise BadOperator("'%s' not a valid operator" % key_op)
+
+            predicates.append((field, op, value))
+
+        return TorrentIterator(self.client).filter(ids, predicates)
+
+if __name__ == "__main__":
+    client = Transmission()
+    for torrent in client:
+        print torrent['name'].encode('utf-8')
