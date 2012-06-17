c608ee2d78713860d8001b7843ceea2a528e92bc
Eric Davis <ed@npri.org>
2012-06-17 10:17:28 -0700

    Add better long_description to setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,26 @@
 from distutils.core import setup
 
+long_description = """
+transmission-fluid is a Python wrapper for Transmission's RPC interface. ::
+
+    >>> from transmission import Transmission
+    >>> client = Transmission()
+    >>> client('torrent-get', ids=range(1,11), fields=['name'])
+    {u'torrents': [
+      {u'name': u'Elvis spotted in Florida.mov'},
+      {u'name': u'Bigfoot sings the hits'},
+      # ...
+      {u'name': u'a-song-of-ice-and-fire_final-chapter.txt'}
+    ]}
+
+See the `README <https://github.com/edavis/transmission-fluid#readme>`_ for more information.
+"""
+
 setup(
     name = "transmission-fluid",
     version = "0.1",
     description = "A Python wrapper for Transmission's RPC interface",
-    long_description = "See the README: https://github.com/edavis/transmission-fluid#readme",
+    long_description = long_description,
     author = "Eric Davis",
     author_email = "ed@npri.org",
     url = "https://github.com/edavis/transmission-fluid",
